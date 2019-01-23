fef6c1de7696788da09b62f36d6f5d0ed6ce9b71
Srujana Gattu <S533622@NWMISSOURI.EDU>
2019-01-23 17:20:15 -0600

    Updated all code

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace w02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Console.WriteLine("Welcome to Doctor's advice!!!");
+            var name = "";
+            var age = 0;
+            var issue = "";
+            var visit = "";
+            var temp = 0.0;
+            var feedback = 'n';
+            var rate = 0.0m;
+            var fee=0.0;
+            var discount=0.0;
+            var visit_before = false;
+            while (name == "")
+            {
+
+                Console.WriteLine("Enter your name");
+                name = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Hi {name} ");
+
+            while (age == 0)
+            {
+
+                Console.WriteLine("Enter your age");
+                age = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            if (age <= 5)
+            {
+                Console.WriteLine("You need to consult doctor directly!!");
+
+            }
+
+            if (age > 5)
+            {
+                Console.WriteLine("What is the health issue you are suffering with?");
+                issue = Console.ReadLine();
+                switch (issue)
+                {
+                    case "fever":
+                        Console.WriteLine("You need to take paracetomol tablets twice a day!");
+                        break;
+                    case "cough":
+                        Console.WriteLine("You need to take KUF-Q tablets twice a day!");
+                        break;
+                    case "cold":
+                        Console.WriteLine("You need to take crocin tablets twice a day!");
+                        break;
+                  default:
+                        Console.WriteLine("\n You need to take advil tablets twice a day!");
+                        break;
+                }
+                Console.WriteLine("What is your body present temperature?");
+                temp = Convert.ToDouble(Console.ReadLine());
+                if(temp <= 99.0 ){
+                 Console.WriteLine("You need to take total of 4 tablets in  two days after your meal!");
+                }
+                if(temp > 99.0){
+
+                    Console.WriteLine("You need to take total of 8 tablets in  four days after your meal!");
+                }
+            
+            
+            
+            Console.WriteLine("Have you used this app before? yes/no");
+            visit = Console.ReadLine();
+            if(visit=="yes")
+            {
+                discount=100;
+                visit_before = true;
+                fee=500-discount;
+            }
+            else{
+
+                visit_before = false;
+                fee=500;
+            }
+            Console.WriteLine($"Your fee is {fee}");
+           
+            Console.WriteLine("Was Doctor's Advice useful? y/n");
+            feedback = Convert.ToChar(Console.ReadLine());
+            }
+            Console.WriteLine("How would you like to rate us?");
+            rate = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Thank you!");
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+Srujana Gattu
+Section 02
+App Name - Doctor's Online Advice
+
+Description - A person can take a online doctor advice by just chating and no need to visit hospital.
+
+Enter your correct name,correct age and describe the issue correctly, it helps doctor to prescribe the correct medicine which makes you cure fast.
+
+Make sure you check your own body temperature and respond to the doctor.
+
+Fee depends on the count of your visit.
+
+Give feedback after using the app.
+
+Thank you!!
